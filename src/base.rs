cfbda05de1ed5bbe72b38f604d8c181ca278aeba
Jon Yoder <jon@yoder.cloud>
2022-03-13 13:08:09 -0400

    Continue implementing ProfileManager - Implement create_profile() and simple access functions - Add stubs for other needed functions

--- src/base.rs
+++ src/base.rs
@@ -1,5 +1,22 @@
+use std::fmt;
 
-// TODO: UserID class
-// TODO: Domain
-// TODO: MAddress class
-// TODO: WAddress class
+#[derive(Debug)]
+pub enum MensagoError {
+	ErrEmptyData,
+	ErrExists,
+	ErrFilesytemError,
+	ErrUnimplemented,
+}
+
+impl std::error::Error for MensagoError {}
+
+impl fmt::Display for MensagoError {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		match self {
+			MensagoError::ErrEmptyData => write!(f, "Empty data error"),
+			MensagoError::ErrExists => write!(f, "Resource already exists"),
+			MensagoError::ErrFilesytemError => write!(f, "Filesystem error"),
+			MensagoError::ErrUnimplemented => write!(f, "Function unimplemented"),
+		}
+	}
+}
--- src/profile.rs
+++ src/profile.rs
@@ -1,4 +1,8 @@
+use std::fs::DirBuilder;
+use std::path::PathBuf;
+use crate::base::*;
 use crate::types::*;
+
 
 /// The Profile type is the client's entry point to interacting with storage. One major point to
 /// note is that it owns the database instance. Unless you are specifically managing profiles, you
@@ -7,13 +11,13 @@ use crate::types::*;
 #[derive(Debug)]
 pub struct Profile {
 	name: String,
-	path: String,
+	path: PathBuf,
 	is_default: bool,
-	uid: UserID,
-	wid: RandomID,
+	uid: Option<UserID>,
+	wid: Option<RandomID>,
 	// db: rusqlite::Connection,
-	domain: Domain,
-	devid: RandomID
+	domain: Option<Domain>,
+	devid: Option<RandomID>
 }
 
 impl Profile {
@@ -25,10 +29,116 @@ impl Profile {
 #[derive(Debug)]
 pub struct ProfileManager {
 	profiles: Vec<Profile>,
-	default_name: String,
-	active_index: u16
+	profile_folder: String,
+	active_index: usize,
+	default_index: usize
 }
 
 impl ProfileManager {
 
+	pub fn activate_profile(name: &str) -> Option<&'static Profile> {
+
+		// TODO: Implement activate_profile()
+
+		None
+	}
+
+	/// Returns the number of profiles in the user's profile folder
+	pub fn count_profiles(&self) -> usize {
+		self.profiles.len()
+	}
+
+	/// Creates a profile of the given name in the user's profile folder. Care should be used with
+	/// spaces and special characters, as the name will be used as the profile's directory name in the
+	/// filesystem. The name 'default' is reserved and may not be used. Note that the profile name is
+	/// not case-sensitive and as such capitalization will be squashed when passed to this function.
+	pub fn create_profile(&mut self, name: &str) -> Result<&Profile, MensagoError> {
+
+		if name.len() == 0 {
+			return Err(MensagoError::ErrEmptyData)
+		}
+
+		let name_squashed = name.to_lowercase();
+		match index_for_name(&name_squashed) {
+			Some(_) => return Err(MensagoError::ErrExists),
+			None => { /* do nothing */ }
+		}
+
+		let mut new_profile_path = PathBuf::from(&self.profile_folder);
+		new_profile_path.push(&name_squashed);
+		match DirBuilder::new().recursive(true).create(new_profile_path.as_path()) {
+			Err(_) => return Err(MensagoError::ErrFilesytemError),
+			Ok(_) => { /* do nothing */ }
+		}
+
+		let mut profile = Profile {
+			name: String::from(name_squashed),
+			path: new_profile_path,
+			is_default: false,
+			uid: None,
+			wid: None,
+			// db: rusqlite::Connection,
+			domain: None,
+			devid: None
+		};
+
+		if self.count_profiles() == 0 {
+			profile.is_default = true;
+			self.default_index = 1;
+		}
+		
+		self.profiles.push(profile);
+
+		Ok(self.profiles.get(self.profiles.len()-1).unwrap())
+	}
+
+	pub fn delete_profile(name: &str) -> Option<MensagoError> {
+
+		// TODO: Implement delete_profile()
+
+		Some(MensagoError::ErrUnimplemented)
+	}
+
+	/// Returns the active profile
+	pub fn get_active_profile(&self) -> Option<&Profile> {
+		self.profiles.get(self.active_index)
+	}
+
+	/// Returns the default profile
+	pub fn get_default_profile(&self) -> Option<&Profile> {
+		self.profiles.get(self.default_index)
+	}
+
+	/// Returns a Vec of all available profiles
+	pub fn get_profiles(&self) -> &Vec<Profile> {
+		&self.profiles
+	}
+
+	pub fn load_profiles(profile_path: Option<&str>) {
+
+		// TODO: Implement load_profiles()
+
+	}
+
+	pub fn rename_profile(oldname: &str, newname: &str) -> Option<MensagoError> {
+
+		// TODO: Implement rename_profile()
+
+		Some(MensagoError::ErrUnimplemented)
+	}
+
+	pub fn set_default_profile(name: &str) -> Option<&'static Profile> {
+
+		// TODO: Implement set_default_profile()
+
+		None
+	}
+
 }
+
+fn index_for_name(name: &str) -> Option<&'static Profile> {
+	
+	// TODO: Implement index_for_name()
+	
+	None
+}	
