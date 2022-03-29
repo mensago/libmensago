d5fc18dff59670d06b625e0a5aa88e74da902279
Jon Yoder <jon@yoder.cloud>
2022-03-29 19:47:41 -0400

    Base85 test actually works

--- base85_test/base85_test.pro
+++ base85_test/base85_test.pro
@@ -7,3 +7,10 @@ CONFIG -= app_bundle
 TEMPLATE = app
 
 SOURCES +=  tst_base85.cpp
+
+win32:CONFIG(release, debug|release): LIBS += -L$$OUT_PWD/../mensago/release/ -lmensago
+else:win32:CONFIG(debug, debug|release): LIBS += -L$$OUT_PWD/../mensago/debug/ -lmensago
+else:unix: LIBS += -L$$OUT_PWD/../mensago/ -lmensago
+
+INCLUDEPATH += $$PWD/../mensago
+DEPENDPATH += $$PWD/../mensago
--- base85_test/tst_base85.cpp
+++ base85_test/tst_base85.cpp
@@ -1,33 +1,54 @@
 #include <QtTest>
 
-// add necessary includes here
+#include "base85.h"
+
+#include <QTest>
+#include <QByteArray>
+#include <QString>
+#include <string>
+
+using namespace std;
 
 class base85_test : public QObject
 {
 	Q_OBJECT
 
-public:
-	base85_test();
-	~base85_test();
-
 private slots:
-	void test_case1();
+	void encode_decode();
 
 };
 
-base85_test::base85_test()
+void base85_test::encode_decode()
 {
+	string testlist[8][2] = {
+		{"a", "VE"},
+		{"aa", "VPO"},
+		{"aaa", "VPRn"},
+		{"aaaa", "VPRom" },
+		{"aaaaa", "VPRomVE"},
+		{"aaaaaa", "VPRomVPO"},
+		{"aaaaaaa", "VPRomVPRn"},
+		{"aaaaaaaa", "VPRomVPRom"}
+	};
 
-}
+	for (uint8_t i = 0; i < 8; i++)
+	{
+		QString encoded;
+		base85encode(QByteArray(testlist[i][0].c_str()), encoded);
+		if (encoded != QString(testlist[i][1].c_str()))
+		{
+			qFatal("Base85encode('%s'): wanted '%s', got '%s'\n", testlist[i][0].c_str(),
+					testlist[i][1].c_str(), qUtf8Printable(encoded));
+		}
 
-base85_test::~base85_test()
-{
-
-}
-
-void base85_test::test_case1()
-{
-
+		QByteArray decoded;
+		base85decode(QString(testlist[i][1].c_str()), decoded);
+		if (decoded != QString(testlist[i][0].c_str()))
+		{
+			qFatal("Base85decode('%s'): wanted '%s', got '%s'\n", testlist[i][1].c_str(),
+					testlist[i][0].c_str(), qUtf8Printable(decoded));
+		}
+	}
 }
 
 QTEST_APPLESS_MAIN(base85_test)
