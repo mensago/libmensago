1c3f679fb05d8af9354ff59721f5b7b908dd0a26
Jon Yoder <jon@yoder.cloud>
2022-04-17 13:06:06 -0400

    *Try* to implement load profiles - Rust can be such a headache sometimes

--- src/profile.rs
+++ src/profile.rs
@@ -1,4 +1,6 @@
-use std::fs::{remove_dir_all, DirBuilder};
+use std::env;
+use std::fs;
+use std::io;
 use std::path::{Path, PathBuf};
 use crate::base::*;
 use crate::types::*;
@@ -22,6 +24,22 @@ pub struct Profile {
 
 impl Profile {
 
+	/// Creates a new profile from a specified path
+	pub fn new(profpath: &Path) -> Profile {
+
+		// TODO: Finish immplementing Profile::new()
+
+		Profile{
+			name: String::from(""),
+			path: PathBuf::from(profpath),
+			is_default: false,
+			uid: None,
+			wid: None,
+			domain: None,
+			devid: None,
+		}
+	}
+
 	/// Sets the profile's internal flag that it is the default profile
 	pub fn set_default(&mut self, is_default: bool) -> Option<String> {
 
@@ -40,14 +58,14 @@ impl Profile {
 #[derive(Debug)]
 pub struct ProfileManager {
 	profiles: Vec<Profile>,
-	profile_folder: String,
-	active_index: usize,
-	default_index: usize
+	profile_folder: PathBuf,
+	active_index: Option<usize>,
+	default_index: Option<usize>
 }
 
 impl ProfileManager {
 
-	pub fn activate_profile(name: &str) -> Option<&'static Profile> {
+	pub fn activate_profile(&mut self, name: &str) -> Option<&'static Profile> {
 
 		// TODO: Implement activate_profile()
 
@@ -81,7 +99,7 @@ impl ProfileManager {
 
 		let mut new_profile_path = PathBuf::from(&self.profile_folder);
 		new_profile_path.push(&name_squashed);
-		match DirBuilder::new().recursive(true).create(new_profile_path.as_path()) {
+		match fs::DirBuilder::new().recursive(true).create(new_profile_path.as_path()) {
 			Err(_) => return Err(MensagoError::ErrFilesytemError),
 			Ok(_) => { /* do nothing */ }
 		}
@@ -99,7 +117,7 @@ impl ProfileManager {
 
 		if self.count_profiles() == 0 {
 			profile.is_default = true;
-			self.default_index = 1;
+			self.default_index = Some(1);
 		}
 		
 		self.profiles.push(profile);
@@ -127,7 +145,7 @@ impl ProfileManager {
 
 		let profile = self.profiles.remove(pindex);
 		if Path::new(profile.path.as_path()).exists() {
-			match remove_dir_all(profile.path.as_path()) {
+			match fs::remove_dir_all(profile.path.as_path()) {
 				Err(e) => return Some(e.to_string()),
 				Ok(_) => { /* continue on */ }
 			}
@@ -142,12 +160,18 @@ impl ProfileManager {
 
 	/// Returns the active profile
 	pub fn get_active_profile(&self) -> Option<&Profile> {
-		self.profiles.get(self.active_index)
+		match self.active_index {
+			Some(v) => self.profiles.get(v),
+			None => None,
+		}
 	}
 
 	/// Returns the default profile
 	pub fn get_default_profile(&self) -> Option<&Profile> {
-		self.profiles.get(self.default_index)
+		match self.default_index {
+			Some(v) => self.profiles.get(v),
+			None => None,
+		}
 	}
 
 	/// Returns a Vec of all available profiles
@@ -155,20 +179,97 @@ impl ProfileManager {
 		&self.profiles
 	}
 
-	pub fn load_profiles(profile_path: Option<&str>) {
+	/// Loads all profiles under the specified path. If None is passed to the function, the profile
+	/// manager will look in ~/.config/mensago on POSIX platforms and %LOCALAPPDATA%\mensago on
+	/// Windows. It returns None on success or a String error.
+	pub fn load_profiles(&mut self, profile_path: Option<&PathBuf>) -> io::Result<()> {
+		
+		self.active_index = None;
 
-		// TODO: Implement load_profiles()
+		self.profile_folder = match profile_path {
+			Some(s) => PathBuf::from(s),
+			None => {
+				if cfg!(windows) {
+					let mut out = PathBuf::new();
+					out.push(&env::var("LOCALAPPDATA").expect("BUG: error getting LOCALAPPDATA"));
+					out.push("mensago");
+					out
+				} else {
+					let mut out = PathBuf::new();
+					out.push(&env::var("LOCALAPPDATA").expect("BUG: error getting LOCALAPPDATA"));
+					out.push("mensago");
+					out
+				}
+			},
+		};
 
+		if !self.profile_folder.exists() {
+			fs::create_dir_all(self.profile_folder.as_path())?;
+		}
+
+		self.profiles.clear();
+		for item in fs::read_dir(self.profile_folder.as_path())? {
+			let entry = item?;
+			let itempath  = entry.path();
+			if !itempath.is_dir() {
+				continue;
+			}
+
+			let profile = Profile::new(&itempath);
+			self.profiles.push(profile);
+			let mut profile = match self.profiles.get(self.profiles.len()-1) {
+				None => panic!("BUG: Out of bounds in load_profiles()"),
+				Some(v) => v
+			};
+
+			if profile.is_default() {
+				match self.default_index {
+					None => {
+						profile.set_default(true);
+						self.default_index = Some(self.profiles.len()-1);
+					},
+					Some(v) => {
+						// If we have more than one profile marked as default, the one in the list
+						// with the lower index retains that status
+						profile.set_default(false);
+					},
+				}
+			}
+		}
+
+		// If we've gotten through the entire loading process and we haven't got a single profile
+		// loaded, then create one
+		if self.profiles.len() == 0 {
+			match self.create_profile("primary") {
+				Ok(v) => {
+					self.set_default_profile("primary");
+				},
+				Err(e) => {
+
+					// TODO: Gotta figure out Rust's stupid complicated error-handling
+					panic!("BUG: Unhandled error in load_profiles()");
+				}
+			}
+		}
+		
+		match self.get_default_profile() {
+			None => { /* We should never be here */ },
+			Some(v) => {
+				self.activate_profile(&v.name);
+			}
+		}
+		
+		Ok(())
 	}
 
-	pub fn rename_profile(oldname: &str, newname: &str) -> Option<MensagoError> {
+	pub fn rename_profile(&mut self, oldname: &str, newname: &str) -> Option<MensagoError> {
 
 		// TODO: Implement rename_profile()
 
 		Some(MensagoError::ErrUnimplemented)
 	}
 
-	pub fn set_default_profile(name: &str) -> Option<&Profile> {
+	pub fn set_default_profile(&mut self, name: &str) -> Option<&Profile> {
 
 		// TODO: Implement set_default_profile()
 
