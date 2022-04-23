5f486129641af1fadc798fcb2869707b25e35052
Jon Yoder <jon@yoder.cloud>
2022-04-23 09:42:13 -0400

    Implement set_credentials()

--- src/auth.rs
+++ src/auth.rs
@@ -9,7 +9,7 @@ pub fn get_credentials(path: PathBuf, waddr: WAddress) -> Result<ArgonHash, Mens
 	dbpath.push("storage.db");
 
 	let passhash: ArgonHash;
-	
+
 	{	// Begin Query
 		// For the fully-commented version of this code, see profile::get_identity()
 		let conn = match rusqlite::Connection::open_with_flags(dbpath, 
@@ -50,4 +50,54 @@ pub fn get_credentials(path: PathBuf, waddr: WAddress) -> Result<ArgonHash, Mens
 	}	// End Query
 	
 	Ok(passhash)
+}
+
+/// Sets the password and hash type for the specified workspace
+pub fn set_credentials(dbpath: PathBuf, waddr: WAddress, pwh: ArgonHash) -> Result<(),MensagoError> {
+
+	let conn = match rusqlite::Connection::open_with_flags(dbpath, 
+		rusqlite::OpenFlags::SQLITE_OPEN_READ_WRITE) {
+			Ok(v) => v,
+			Err(e) => {
+				return Err(MensagoError::ErrDatabaseException(e.to_string()))
+			}
+		};
+	
+	
+	// Check to see if the workspace address passed tothe function exists
+	let mut stmt = match conn.prepare("SELECT wid FROM workspaces WHERE wid=?! AND domain=?2") {
+		Ok(v) => v,
+		Err(e) => {
+			return Err(MensagoError::ErrDatabaseException(e.to_string()))
+		}
+	};
+		
+	let mut rows = match stmt.query([waddr.get_wid().as_string(), waddr.get_domain().as_string()]) {
+		Ok(v) => v,
+		Err(e) => {
+			return Err(MensagoError::ErrDatabaseException(e.to_string()))
+		}
+	};
+
+	match rows.next() {
+		Ok(optrow) => {
+			match optrow {
+				// This means that the workspace ID wasn't found
+				None => { return Err(MensagoError::ErrNotFound) },
+				Some(_) => { /* Do nothing. The workspace exists. */ }
+			}
+		},
+		Err(e) => {
+			return Err(MensagoError::ErrDatabaseException(e.to_string()))
+		}
+	};
+
+	match conn.execute("UPDATE workspaces SET password=?1,pwhashtype=?2 WHERE wid=?3 AND domain=?4",
+			&[pwh.get_hash(), pwh.get_hashtype(), waddr.get_wid().as_string(),
+			waddr.get_domain().as_string()]) {
+		Ok(_) => { return Ok(()) },
+		Err(e) => {
+			return Err(MensagoError::ErrDatabaseException(e.to_string()))
+		}
+	}
 }
