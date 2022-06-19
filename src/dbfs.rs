402eb30d91d16131ef686ee82b30803abaeef0df
Jon Yoder <jon@yoder.cloud>
2022-06-19 13:49:07 -0400

    Finish implementing DBPath

--- src/dbfs.rs
+++ src/dbfs.rs
@@ -1,6 +1,12 @@
+//! This module implements basic filesystem operations inside the database. This is because
+//! the files themselves are stored in the database for (a) data protection and (b) easy search.
+//! Only attachments are stored outside the database, and this is for bloat protection and ease of 
+//! access. The files' names are the same as those on the server to make tracking them easier.
+//! Folder names OTOH are stored in the database using the user-facing names (inbox, notes, etc.)
 use lazy_static::lazy_static;
 use libkeycard::*;
 use regex::Regex;
+use std::char;
 use std::fmt;
 use crate::base::*;
 
@@ -14,6 +20,8 @@ lazy_static! {
 /// Represents a path in the database virtual filesystem. Slashes are the path separator character.
 /// The only printable characters not permitted in these paths are backslashes and control
 /// characters. Trailing slashes and leading and trailing whitespace are stripped to avoid problems.
+/// A folder path could look like '/Inbox/Bills' and a file or message could be something like
+/// '/Archive/Bills/a2de7cd4-deee-4a61-aef2-71785ab0d339'.
 #[derive(Debug, Clone)]
 pub struct DBPath {
 	path: String,
@@ -33,18 +41,57 @@ impl DBPath {
 		DBPath { path: String::new() }
 	}
 
+	/// Creates a DBPath from a string. ErrBadValue is returned if the value contains illegal
+	/// characters.
 	pub fn from(s: &str) -> Result<DBPath, MensagoError> {
 
-		let trimmed = s.trim().trim_end_matches("/");
+		let mut out = DBPath { path: String::new() };
+		out.set(s)?;
+		Ok(out)
+	}
+
+	/// Returns the name of the entry represented by the path
+	pub fn basename(&self) -> &str {
+
+		if self.path.len() == 0 {
+			return &self.path
+		}
+
+		let parts: Vec::<&str> = self.path.split("/").collect();
+		parts[parts.len() - 1]
+	}
+
+	/// Appends the supplied path to the object
+	pub fn push(&mut self, s: &str) -> Result<(), MensagoError> {
+
+		if self.path.len() == 0 {
+			return self.set(s)
+		}
+
+		let trimmed = s.trim_matches(char::is_whitespace).trim_matches('/');
 		if BAD_PATH_CHARS_PATTERN.is_match(trimmed) {
 			return Err(MensagoError::ErrBadValue)
 		}
 
-		Ok(DBPath { path: String::from(trimmed) })
+		self.path = format!("{}/{}", self.path, trimmed);
+		Ok(())
+	}
+
+	/// Sets the object to the supplied value
+	pub fn set(&mut self, s: &str) -> Result<(), MensagoError> {
+
+		let trimmed = s.trim_matches(char::is_whitespace).trim_matches('/');
+		if BAD_PATH_CHARS_PATTERN.is_match(trimmed) {
+			return Err(MensagoError::ErrBadValue)
+		}
+
+		self.path = format!("/{}/{}", self.path, trimmed);
+		Ok(())
 	}
 }
 
 /// Represents the mapping of a server-side path to one in the database virtual filesystem
+#[derive(Debug, Clone)]
 pub struct FolderMap {
 	fid: RandomID,
 	address: WAddress,
