e2ef62a1e37443783394d2e25bcd4fbdaf106132
Jon Yoder <jon@yoder.cloud>
2022-07-14 16:34:06 -0400

    Begin implementing getwid()

--- src/commands/iscmds.rs
+++ src/commands/iscmds.rs
@@ -1,9 +1,24 @@
 use crate::base::*;
+use crate::commands::servermsg::*;
 use libkeycard::*;
 use std::net::TcpStream;
 
-pub fn getwid(conn: &mut TcpStream, uid: &UserID, domain: &Domain)
+pub fn getwid(conn: &mut TcpStream, uid: &UserID, domain: Option<&Domain>)
 -> Result<RandomID, MensagoError> {
 
+	let req = ClientRequest::from(
+		"GETWID", vec![
+			("User-ID", uid.as_string()),
+		]
+	);
+
+	if domain.is_some() {
+		req.data.insert(String::from("Domain"), String::from(domain.unwrap().as_string()));
+	}
+
+	let resp = ServerResponse::receive(conn)?;
+
+	// TODO: finish implementing getwid()
+	
 	Err(MensagoError::ErrUnimplemented)
 }
