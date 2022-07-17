2840b9ef67ed0abc37eea7a36c7b8c1d2b62bb88
Jon Yoder <jon@yoder.cloud>
2022-07-17 13:21:41 -0400

    Begin writing integration test setup code

--- integration_tests/tests/common.rs
+++ integration_tests/tests/common.rs
@@ -1,2 +1,45 @@
 //! This module contains setup functions needed by the integration tests
 
+// THESE KEYS ARE STORED ON GITLAB! DO NOT USE THESE FOR ANYTHING EXCEPT UNIT TESTS!!
+
+// Test Organization Information
+
+// Name: Example.com
+// Contact-Admin: ae406c5e-2673-4d3e-af20-91325d9623ca/acme.com
+// Support and Abuse accounts are forwarded to Admin
+// Language: en
+
+// Initial Organization Primary Signing Key: {UNQmjYhz<(-ikOBYoEQpXPt<irxUF*nq25PoW=_
+// Initial Organization Primary Verification Key: r#r*RiXIN-0n)BzP3bv`LA&t4LFEQNF0Q@$N~RF*
+// Initial Organization Primary Verification Key Hash: 
+// BLAKE2B-256:ag29av@TUvh-V5KaB2l}H=m?|w`}dvkS1S1&{cMo
+
+// Initial Organization Encryption Key: SNhj2K`hgBd8>G>lW$!pXiM7S-B!Fbd9jT2&{{Az
+// Initial Organization Encryption Key Hash: BLAKE2B-256:-Zz4O7J;m#-rB)2llQ*xTHjtblwm&kruUVa_v(&W
+// Initial Organization Decryption Key: WSHgOhi+bg=<bO^4UoJGF-z9`+TBN{ds?7RZ;w3o
+
+// THESE KEYS ARE STORED ON GITLAB! DO NOT USE THESE FOR ANYTHING EXCEPT UNIT TESTS!!
+
+use libmensago::*;
+use std::path::PathBuf;
+
+/// Loads the Mensago server configuration from the config file
+pub fn load_server_config() -> Result<(), MensagoError> {
+
+	let config_file_path: PathBuf;
+
+	if cfg!(windows) {
+		config_file_path = PathBuf::from("C:\\ProgramData\\mensagod\\serverconfig.toml");
+	} else {
+		config_file_path = PathBuf::from("/etc/mensagod/serverconfig.toml");
+	}
+
+	if config_file_path.exists() {
+		
+	} else {
+		return Err(MensagoError::ErrNotFound)
+	}
+
+	Ok(())
+}
+
