902671b09402b2d971adcbe14c6ae52333ac707d
Jon Yoder <jon@yoder.cloud>
2022-07-20 19:51:55 -0400

    Add stubs for the IS commands needing to be implemented next

--- libmensago/src/commands/iscmds.rs
+++ libmensago/src/commands/iscmds.rs
@@ -1,8 +1,20 @@
 use crate::base::*;
 use crate::commands::servermsg::*;
 use crate::conn::*;
+use eznacl::*;
 use libkeycard::*;
 
+/// Completes the login process by submitting the device ID and responding to the server's device
+/// challenge.
+pub fn device(conn: &mut ServerConnection, devid: &RandomID, devpair: &EncryptionPair)
+-> Result<(), MensagoError> {
+
+	// TODO: implement iscmds::device()
+	return Err(MensagoError::ErrUnimplemented)
+}
+
+/// Looks up a workspace ID based on the specified user ID and optional domain. If the domain is
+/// not specified, the organization's domain is used.
 pub fn getwid(conn: &mut ServerConnection, uid: &UserID, domain: Option<&Domain>)
 -> Result<RandomID, MensagoError> {
 
@@ -32,6 +44,8 @@ pub fn getwid(conn: &mut ServerConnection, uid: &UserID, domain: Option<&Domain>
 	}
 }
 
+/// Finds out if an entry index is current. If workspace ID is omitted, this command checks the
+/// index for the organization's keycard.
 pub fn iscurrent(conn: &mut ServerConnection, index: usize, wid: Option<RandomID>)
 -> Result<bool, MensagoError> {
 
@@ -56,4 +70,29 @@ pub fn iscurrent(conn: &mut ServerConnection, index: usize, wid: Option<RandomID
 	}
 	
 	Ok(resp.data.get("Is-Current").unwrap() == "YES")
+}
+
+/// Starts the login process by submitting the desired workspace ID.
+pub fn login(conn: &mut ServerConnection, wid: &RandomID, serverkey: &CryptoString)
+-> Result<(), MensagoError> {
+
+	// TODO: implement iscmds::login()
+	return Err(MensagoError::ErrUnimplemented)
+}
+
+/// Continues the login process by sending a password hash for the workspace
+pub fn password(conn: &mut ServerConnection, pwhash: &ArgonHash)
+-> Result<(), MensagoError> {
+
+	// TODO: implement iscmds::password()
+	return Err(MensagoError::ErrUnimplemented)
+}
+
+/// Finishes the registration of a workspace. The address may be a regular Mensago address or it 
+/// can be a workspace address.
+pub fn regcode(conn: &mut ServerConnection, address: &MAddress, code: &str, pwhash: &ArgonHash, 
+devid: &RandomID, devpubkey: &CryptoString) -> Result<(), MensagoError> {
+
+	// TODO: implement iscmds::regcode()
+	return Err(MensagoError::ErrUnimplemented)
 }
