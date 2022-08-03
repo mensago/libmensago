362e983405953cfb26b02de7b7acca3720ba4cc2
Jon Yoder <jon@yoder.cloud>
2022-08-03 17:44:11 -0400

    implement test_register()

--- integration_tests/tests/iscmds.rs
+++ integration_tests/tests/iscmds.rs
@@ -193,4 +193,79 @@ mod tests {
 	}
 
 	// login() is tested by common.rs::test_regcode_user()
+
+	#[test]
+	fn test_register() -> Result<(), MensagoError> {
+		let testname = "test_register";
+
+		let mut config = load_server_config(true)?;
+		let mut db = match setup_test(&config) {
+			Ok(v) => v,
+			Err(e) => {
+				return Err(MensagoError::ErrProgramException(
+					format!("{}: setup_test error: {}", testname, e.to_string())
+				))
+			}
+		};
+		match init_server(&mut db) {
+			Ok(_) => (),
+			Err(e) => {
+				return Err(MensagoError::ErrProgramException(
+					format!("{}: init_server error: {}", testname, e.to_string())
+				))
+			}
+		}
+		let profile_folder = match setup_profile_base(testname) {
+			Ok(v) => v,
+			Err(e) => {
+				return Err(MensagoError::ErrProgramException(
+					format!("{}: setup_profile_base error: {}", testname, e.to_string())
+				))
+			}
+		};
+		let pwhash = match setup_profile(&profile_folder, &mut config, &ADMIN_PROFILE_DATA) {
+			Ok(v) => v,
+			Err(e) => {
+				return Err(MensagoError::ErrProgramException(
+					format!("{}: setup_profile error: {}", testname, e.to_string())
+				))
+			}
+		};
+
+		let mut profman = ProfileManager::new(&PathBuf::from(&profile_folder));
+		match profman.load_profiles(Some(&PathBuf::from(&profile_folder))) {
+			Ok(_) => (),
+			Err(e) => {
+				return Err(MensagoError::ErrProgramException(
+					format!("{}: load_profiles error: {}", testname, e.to_string())
+				))
+			}
+		};
+
+		let profile = profman.get_active_profile().unwrap();
+		let mut conn = ServerConnection::new();
+		let port = config["network"]["port"].as_integer().unwrap();
+		match conn.connect(config["network"]["listen_ip"].as_str().unwrap(), &port.to_string()) {
+			Ok(_) => (),
+			Err(e) => {
+				return Err(MensagoError::ErrProgramException(
+					format!("{}: error connecting to server: {}", testname, e.to_string())
+				))
+			}
+		}
+		
+		let devpair = EncryptionPair::generate("CURVE25519")?;
+		match register(&mut conn, UserID::from("csimons").as_ref(), &pwhash.to_string(), 
+			profile.devid.as_ref().unwrap(), &devpair.get_public_key()) {
+			Ok(_) => (),
+			Err(e) => {
+				return Err(MensagoError::ErrProgramException(
+					format!("{}: register error: {}", testname, e.to_string())
+				))
+			}
+		}
+		conn.disconnect()?;
+
+		Ok(())
+	}
 }
