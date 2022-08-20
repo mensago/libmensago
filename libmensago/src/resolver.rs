2c2427bb007dc7cf4555c799160c91fce2c8b22b
Jon Yoder <jon@yoder.cloud>
2022-08-20 15:37:38 -0400

    Finish IPv4 and implement IPv6 + tests

--- libmensago/src/resolver.rs
+++ libmensago/src/resolver.rs
@@ -1,4 +1,4 @@
-use std::net::{IpAddr, SocketAddr, ToSocketAddrs};
+use std::net::{IpAddr, SocketAddr, ToSocketAddrs, Ipv4Addr, Ipv6Addr};
 use eznacl::CryptoString;
 use libkeycard::Domain;
 use crate::MensagoError;
@@ -6,6 +6,7 @@ use crate::MensagoError;
 /// The DNSHandler trait is an abstraction trait for easy unit testing
 pub trait DNSHandlerT {
 	fn lookup_a(&self, d: &Domain) -> Result<IpAddr, MensagoError>;
+	fn lookup_aaaa(&self, d: &Domain) -> Result<IpAddr, MensagoError>;
 }
 
 /// The DNSHandler type provides a simple DNS API specific to the needs of Mensago clients
@@ -28,8 +29,20 @@ impl DNSHandlerT for DNSHandler {
 		let addresses: Vec<SocketAddr> = format!("{}:443", d.as_string()).to_socket_addrs()?.collect();
 
 		for a in addresses {
-			println!("{:?}", a);
 			if a.is_ipv4() {
+				return Ok(a.ip())
+			}
+		}
+		
+		Err(MensagoError::ErrNotFound)
+	}
+
+	fn lookup_aaaa(&self, d: &Domain) -> Result<IpAddr, MensagoError> {
+		
+		let addresses: Vec<SocketAddr> = format!("{}:443", d.as_string()).to_socket_addrs()?.collect();
+
+		for a in addresses {
+			if a.is_ipv6() {
 				return Ok(a.ip())
 			}
 		}
@@ -43,13 +56,22 @@ pub struct FakeDNSHandler {
 
 }
 
+impl FakeDNSHandler {
+
+	pub fn new() -> FakeDNSHandler {
+		FakeDNSHandler {  }
+	}
+
+}
+
 impl DNSHandlerT for FakeDNSHandler {
 
-	fn lookup_a(&self, d: &Domain) -> Result<IpAddr, MensagoError> {
+	fn lookup_a(&self, _d: &Domain) -> Result<IpAddr, MensagoError> {
+		Ok(IpAddr::V4(Ipv4Addr::new(127,0,0,1)))
+	}
 
-		// TODO: Implement FakeDNSHandler::lookup_a()
-
-		Err(MensagoError::ErrUnimplemented)
+	fn lookup_aaaa(&self, _d: &Domain) -> Result<IpAddr, MensagoError> {
+		Ok(IpAddr::V6(Ipv6Addr::new(0,0,0,0,0,0,0,1)))
 	}
 }
 
@@ -90,10 +112,10 @@ impl KCResolver {
 mod test {
     use crate::*;
 	use libkeycard::Domain;
-	use std::net::{IpAddr, Ipv4Addr};
+	use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 
 	#[test]
-	fn test_real_lookup_a() -> Result<(), MensagoError> {
+	fn test_lookup_a() -> Result<(), MensagoError> {
 		let testname = "test_real_lookup_a";
 
 		let example_com = IpAddr::V4(Ipv4Addr::new(93,184,216,34));
@@ -107,6 +129,49 @@ mod test {
 			}
 		};
 		assert_eq!(returned_ip, example_com);
+
+		let loopback = IpAddr::V4(Ipv4Addr::new(127,0,0,1));
+		let fake_handler = FakeDNSHandler::new();
+		let returned_ip = match fake_handler.lookup_a(&Domain::from("example.com").unwrap()) {
+			Ok(v) => v,
+			Err(e) => {
+				return Err(MensagoError::ErrProgramException(
+					format!("{}: error faking address lookup: {}", testname, e.to_string())
+				))
+			}
+		};
+		assert_eq!(returned_ip, loopback);
+
+		Ok(())
+	}
+
+	#[test]
+	fn test_lookup_aaaa() -> Result<(), MensagoError> {
+		let testname = "test_real_lookup_aaaa";
+
+		let example_com = IpAddr::V6(Ipv6Addr::new(0x2606,0x2800,0x220,1,0x248,0x1893,0x25c8,0x1946));
+		let real_handler = DNSHandler::new();
+		let returned_ip = match real_handler.lookup_aaaa(&Domain::from("example.com").unwrap()) {
+			Ok(v) => v,
+			Err(e) => {
+				return Err(MensagoError::ErrProgramException(
+					format!("{}: error looking up address: {}", testname, e.to_string())
+				))
+			}
+		};
+		assert_eq!(returned_ip, example_com);
+
+		let loopback = IpAddr::V6(Ipv6Addr::new(0,0,0,0,0,0,0,1));
+		let fake_handler = FakeDNSHandler::new();
+		let returned_ip = match fake_handler.lookup_aaaa(&Domain::from("example.com").unwrap()) {
+			Ok(v) => v,
+			Err(e) => {
+				return Err(MensagoError::ErrProgramException(
+					format!("{}: error faking address lookup: {}", testname, e.to_string())
+				))
+			}
+		};
+		assert_eq!(returned_ip, loopback);
 
 		Ok(())
 	}
