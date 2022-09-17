aabff324a81af4617fa2fc999df93ad1d9560666
Jon Yoder <jon@yoder.cloud>
2022-09-17 09:48:47 -0400

    Move to using rustfmt

--- integration_tests/tests/client.rs
+++ integration_tests/tests/client.rs
@@ -1,85 +1,97 @@
-#[cfg(test)] 
+#[cfg(test)]
 mod tests {
-	use libkeycard::*;
-	use libmensago::*;
-	use crate::common::*;
-	
-	#[test]
-	fn test_preregister() -> Result<(), MensagoError> {
-		let testname = "test_preregister";
+    use crate::common::*;
+    use libkeycard::*;
+    use libmensago::*;
 
-		// The list of full data is as follows:
-		// let (config, db, dbdata, profile_folder, pwhash, profman, mut conn, admin_regdata) = 
-		// 	full_test_setup(testname)?;
-		let (_, _, _, _, _, _, mut conn, _) = full_test_setup(testname)?;
+    #[test]
+    fn test_preregister() -> Result<(), MensagoError> {
+        let testname = "test_preregister";
 
-		// TODO: implement test_preregister
+        // The list of full data is as follows:
+        // let (config, db, dbdata, profile_folder, pwhash, profman, mut conn, admin_regdata) =
+        // 	full_test_setup(testname)?;
+        let (_, _, _, _, _, _, mut conn, _) = full_test_setup(testname)?;
 
-		conn.disconnect()
-	}
+        // TODO: implement test_preregister
 
-	#[test]
-	fn test_register() -> Result<(), MensagoError> {
-		let testname = "test_register";
+        conn.disconnect()
+    }
 
-		// The list of full data is as follows:
-		// let (config, db, dbdata, profile_folder, pwhash, profman, mut conn, admin_regdata) = 
-		// 	full_test_setup(testname)?;
-		let (_, _, _, profile_folder, _, _, mut conn, _) = full_test_setup(testname)?;
+    #[test]
+    fn test_register() -> Result<(), MensagoError> {
+        let testname = "test_register";
 
-		conn.disconnect()?;
+        // The list of full data is as follows:
+        // let (config, db, dbdata, profile_folder, pwhash, profman, mut conn, admin_regdata) =
+        // 	full_test_setup(testname)?;
+        let (_, _, _, profile_folder, _, _, mut conn, _) = full_test_setup(testname)?;
 
-		let mut client = match Client::new(&profile_folder.to_string()) {
-			Ok(v) => v,
-			Err(e) => {
-				return Err(MensagoError::ErrProgramException(
-					format!("{}: error initializing client: {}", testname, e.to_string())
-				))
-			}
-		};
-		client.enable_test_mode(true);
+        conn.disconnect()?;
 
-		match client.get_profile_manager().create_profile("user") {
-			Ok(_) => (),
-			Err(e) => {
-				return Err(MensagoError::ErrProgramException(
-					format!("{}: error creating test user profile: {}", testname, e.to_string())
-				))
-			}
-		}
+        let mut client = match Client::new(&profile_folder.to_string()) {
+            Ok(v) => v,
+            Err(e) => {
+                return Err(MensagoError::ErrProgramException(format!(
+                    "{}: error initializing client: {}",
+                    testname,
+                    e.to_string()
+                )))
+            }
+        };
+        client.enable_test_mode(true);
 
-		match client.get_profile_manager().activate_profile("user") {
-			Ok(_) => (),
-			Err(e) => {
-				return Err(MensagoError::ErrProgramException(
-					format!("{}: error activating test user profile: {}", testname, e.to_string())
-				))
-			}
-		}
+        match client.get_profile_manager().create_profile("user") {
+            Ok(_) => (),
+            Err(e) => {
+                return Err(MensagoError::ErrProgramException(format!(
+                    "{}: error creating test user profile: {}",
+                    testname,
+                    e.to_string()
+                )))
+            }
+        }
 
-		let example_com = Domain::from("example.com").unwrap();
-		match client.connect(&example_com) {
-			Ok(_) => (),
-			Err(e) => {
-				return Err(MensagoError::ErrProgramException(
-					format!("{}: error connecting to example.com: {}", testname, e.to_string())
-				))
-			}
-		}
+        match client.get_profile_manager().activate_profile("user") {
+            Ok(_) => (),
+            Err(e) => {
+                return Err(MensagoError::ErrProgramException(format!(
+                    "{}: error activating test user profile: {}",
+                    testname,
+                    e.to_string()
+                )))
+            }
+        }
 
-		match client.register(&example_com, "MyS3cretPassw*rd", 
-			Some(&UserID::from("csimons").unwrap())) {
-			Ok(v) => v,
-			Err(e) => {
-				return Err(MensagoError::ErrProgramException(
-					format!("{}: error registering test user: {}", testname, e.to_string())
-				))
-			}
-		};
+        let example_com = Domain::from("example.com").unwrap();
+        match client.connect(&example_com) {
+            Ok(_) => (),
+            Err(e) => {
+                return Err(MensagoError::ErrProgramException(format!(
+                    "{}: error connecting to example.com: {}",
+                    testname,
+                    e.to_string()
+                )))
+            }
+        }
 
-		client.disconnect()?;
+        match client.register(
+            &example_com,
+            "MyS3cretPassw*rd",
+            Some(&UserID::from("csimons").unwrap()),
+        ) {
+            Ok(v) => v,
+            Err(e) => {
+                return Err(MensagoError::ErrProgramException(format!(
+                    "{}: error registering test user: {}",
+                    testname,
+                    e.to_string()
+                )))
+            }
+        };
 
-		Ok(())
-	}
+        client.disconnect()?;
+
+        Ok(())
+    }
 }
-
