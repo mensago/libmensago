b0ca0764625118059ea9990d47ab3fe126b73700
Jon Yoder <jon@yoder.cloud>
2022-09-27 09:41:47 -0400

    Add stub for test_client_update_keycard()

--- integration_tests/tests/client.rs
+++ integration_tests/tests/client.rs
@@ -218,4 +218,67 @@ mod tests {
 
         Ok(())
     }
+
+    #[test]
+    fn test_client_update_keycard() -> Result<(), MensagoError> {
+        let testname = "test_client_update_keycard";
+
+        // The list of full data is as follows:
+        // let (config, db, dbdata, profile_folder, pwhash, profman, mut conn, admin_regdata) =
+        // 	full_test_setup(testname)?;
+        let (_, _, _, profile_folder, _, _, mut conn, _) = full_test_setup(testname)?;
+        conn.disconnect()?;
+
+        let dns = FakeDNSHandler::new();
+        let mut client = match Client::new(&profile_folder.to_string(), Box::new(dns), false) {
+            Ok(v) => v,
+            Err(e) => {
+                return Err(MensagoError::ErrProgramException(format!(
+                    "{}: error initializing client: {}",
+                    testname,
+                    e.to_string()
+                )))
+            }
+        };
+
+        match client.get_profile_manager().create_profile("user") {
+            Ok(_) => (),
+            Err(e) => {
+                return Err(MensagoError::ErrProgramException(format!(
+                    "{}: error creating test user profile: {}",
+                    testname,
+                    e.to_string()
+                )))
+            }
+        }
+
+        match client.get_profile_manager().activate_profile("user") {
+            Ok(_) => (),
+            Err(e) => {
+                return Err(MensagoError::ErrProgramException(format!(
+                    "{}: error activating test user profile: {}",
+                    testname,
+                    e.to_string()
+                )))
+            }
+        }
+
+        let example_com = Domain::from("example.com").unwrap();
+        match client.connect(&example_com) {
+            Ok(_) => (),
+            Err(e) => {
+                return Err(MensagoError::ErrProgramException(format!(
+                    "{}: error connecting to example.com: {}",
+                    testname,
+                    e.to_string()
+                )))
+            }
+        }
+
+        // TODO: finish test_client_update_keycard()
+
+        client.disconnect()?;
+
+        Ok(())
+    }
 }
