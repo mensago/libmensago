9319f05976c9b09cd30bbf05c14d6f9c6effc02f
Jon Yoder <jon@yoder.cloud>
2022-10-03 16:23:12 -0400

    Begin implementing (and debugging) check_db_value()

--- integration_tests/tests/models.rs
+++ integration_tests/tests/models.rs
@@ -45,6 +45,35 @@ mod tests {
         Ok((config, pwhash, profman))
     }
 
+    fn check_db_value(
+        conn: &mut rusqlite::Connection,
+        tablename: &str,
+        id: &RandomID,
+        column: &str,
+        value: &str,
+    ) -> Result<(), MensagoError> {
+        if tablename.len() == 0 || column.len() == 0 {
+            return Err(MensagoError::ErrEmptyData);
+        }
+
+        let mut stmt = conn.prepare("SELECT ?1 FROM ?2 WHERE id = ?3")?;
+        let dbvalue = match stmt.query_row(&[column, tablename, &id.to_string()], |row| {
+            Ok(row.get::<usize, String>(0).unwrap())
+        }) {
+            Ok(v) => v,
+            Err(e) => return Err(MensagoError::ErrDatabaseException(e.to_string())),
+        };
+
+        if dbvalue == value {
+            return Ok(());
+        }
+
+        Err(MensagoError::ErrDatabaseException(format!(
+            "wanted {}, got {}",
+            value, dbvalue,
+        )))
+    }
+
     #[test]
     fn test_namepartmodel() -> Result<(), MensagoError> {
         let testname = "test_namepartmodel";
@@ -56,14 +85,25 @@ mod tests {
         let profile = profman.get_active_profile().unwrap();
         let mut db = profile.open_storage()?;
 
-        let model =
-            NamePartModel::new(&RandomID::from("00000000-1111-2222-3333-444444444444").unwrap());
+        let modelid = RandomID::from("00000000-1111-2222-3333-444444444444").unwrap();
+        let model = NamePartModel::new(&modelid);
 
         match model.add_to_db(&mut db) {
             Ok(_) => (),
             Err(e) => {
                 return Err(MensagoError::ErrProgramException(format!(
                     "{}: add_to_db() error: {}",
+                    testname,
+                    e.to_string()
+                )))
+            }
+        }
+
+        match check_db_value(&mut db, "contact_nameparts", &modelid, "priority", "0") {
+            Ok(_) => (),
+            Err(e) => {
+                return Err(MensagoError::ErrProgramException(format!(
+                    "{}: add_to_db value check error for priority: {}",
                     testname,
                     e.to_string()
                 )))
