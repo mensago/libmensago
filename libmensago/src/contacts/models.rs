17045e5c1454148aa55f2104cefd8fa1319798dd
Jon Yoder <jon@yoder.cloud>
2022-10-07 15:16:38 -0400

    Implement get_names() and get_ids()

--- libmensago/src/contacts/models.rs
+++ libmensago/src/contacts/models.rs
@@ -1765,4 +1765,65 @@ impl ContactModel {
             annotations: None,
         })
     }
+
+    /// Returns the IDs of all contacts in the database.
+    ///
+    /// Because file attachments can make ContactModels very large and a user may have thousands of
+    /// contacts, this method and `get_names()` are the two ways of getting information
+    /// about all of the user's contacts at once and dealing with them iteratively. Generally
+    /// speaking, you probably want `get_names()`.
+    pub fn get_ids(conn: &mut rusqlite::Connection) -> Result<Vec<RandomID>, MensagoError> {
+        let mut ids = Vec::<RandomID>::new();
+
+        let mut stmt = match conn.prepare("SELECT id FROM contacts") {
+            Ok(v) => v,
+            Err(e) => return Err(MensagoError::ErrDatabaseException(e.to_string())),
+        };
+
+        let mut rows = match stmt.query([]) {
+            Ok(v) => v,
+            Err(e) => return Err(MensagoError::ErrDatabaseException(e.to_string())),
+        };
+
+        let mut option_row = match rows.next() {
+            Ok(v) => v,
+            Err(e) => return Err(MensagoError::ErrDatabaseException(e.to_string())),
+        };
+
+        while option_row.is_some() {
+            let row = option_row.unwrap();
+            let partid = match RandomID::from(&row.get::<usize, String>(0).unwrap()) {
+                Some(v) => v,
+                None => {
+                    return Err(MensagoError::ErrDatabaseException(format!(
+                        "Bad contact model ID {} in contacts",
+                        &row.get::<usize, String>(0).unwrap()
+                    )))
+                }
+            };
+            ids.push(partid);
+
+            option_row = match rows.next() {
+                Ok(v) => v,
+                Err(e) => return Err(MensagoError::ErrDatabaseException(e.to_string())),
+            };
+        }
+
+        Ok(ids)
+    }
+
+    /// This convenience method returns a list of NameModels representing all the user's contacts.
+    ///
+    /// Because file attachments can make ContactModels very large and a user may have thousands of
+    /// contacts, this method and `get_ids()` are the two ways of getting information about all of
+    /// the user's contacts at once and dealing with them iteratively.
+    #[inline]
+    pub fn get_names(conn: &mut rusqlite::Connection) -> Result<Vec<NameModel>, MensagoError> {
+        let mut out = Vec::<NameModel>::new();
+
+        for id in ContactModel::get_ids(conn)? {
+            out.push(NameModel::load_from_db(&id, conn)?)
+        }
+        Ok(out)
+    }
 }
